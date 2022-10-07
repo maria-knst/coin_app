84c8b358281203756363a9b3c596495f1521235c
Maria <konstantinovamaria25@gmail.com>
2022-10-07 14:36:33 +0300

    Add ability to store briefcase in localStorage.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,29 @@
 import { createStore } from "redux";
 import { reduser, initialState } from "../ducks/briefcase";
 
-export default createStore(reduser, initialState );
+function saveToLocalStorage(state) {
+    try {
+        const serialisedState = JSON.stringify(state);
+        localStorage.setItem("persistantState", serialisedState);
+    } catch (e) {
+        console.warn(e);
+    }
+}
+
+function loadFromLocalStorage() {
+    try {
+        const serialisedState = localStorage.getItem("persistantState");
+        if (serialisedState === null) return initialState;
+        return JSON.parse(serialisedState);
+    } catch (e) {
+        console.warn(e);
+        return undefined;
+    }
+}
+
+const store =  createStore(reduser, loadFromLocalStorage());
+
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
+
+export default store;
